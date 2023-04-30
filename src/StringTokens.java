51e3ce53c1c3c9ffa6f43cf01dffdcff772d494d
Jānis Ruduks <janis.ruduks157@gmail.com>
2023-04-30 20:24:50 +0300

    not done

--- src/StringTokens.java
+++ src/StringTokens.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class StringTokens {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringTokens {
+    public static void tokens(){
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        scanner.close();
+
+        String[] tokens = s.split(" !,?._'@");
+
+        System.out.println(Arrays.toString(tokens));
+    }
 }
